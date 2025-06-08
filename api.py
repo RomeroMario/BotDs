86c37c4f41c5bc460c95b6d04a67a21e6df95087
Mario Alberto Romero <83764@sistemas.frc.utn.edu.ar>
2025-06-07 22:45:30 -0300

    añadido control de db

--- api.py
+++ api.py
@@ -1,7 +1,7 @@
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi import FastAPI
 from datetime import datetime
-from db import guardarConfesión
+from db import guardarConfesión, modificarEstado,obtenerConfesiones
 
 app = FastAPI()
 
@@ -23,33 +23,11 @@ async def read_root():
 
 @app.get("/confesiones")
 async def get_confesiones():
-    try:
-        with open("/etc/secrets/confesiones.txt", "r", encoding="utf-8") as f:
-            lineas = f.readlines()
-            if lineas:
-                return lineas
-            else:
-                return []
-    except FileNotFoundError:
-        return []
+    return obtenerConfesiones()
 
 @app.post("/lectura")
 async def lectura(id: int):
-    try:
-        with open("/etc/secrets/confesiones.txt", "r", encoding="utf-8") as f:
-            lineas = f.readlines()
-            if lineas:
-                for i, linea in enumerate(lineas):
-                    if int(linea.split("&")[0]) == id:
-                        lineas[i] = linea.replace("False", "True")
-                        break
-                with open("confesiones.txt", "w", encoding="utf-8") as f:
-                    f.writelines(lineas)
-                return {"message": "Confesión marcada como leída"}
-            else:
-                return {"message": "No se encontraron confesiones"}
-    except FileNotFoundError:
-        return {"message": "No se encontraron confesiones"}
+    modificarEstado(id)
 
 
 
--- db.py
+++ db.py
@@ -4,5 +4,17 @@ from supabase import create_client, Client
 supabase: Client = create_client(SB_URL, SB_KEY)
 
 def guardarConfesión(cuerpo: str, fecha: str, leido: bool):
-    nuevo = {"cuerpo": cuerpo, "fecha": fecha, "leido": leido}
+    nuevo = {"cuerpo": cuerpo, "fecha": fecha, "estado": leido}
     supabase.table("confesion").insert(nuevo).execute()
+    
+def modificarEstado(id: int):
+    # Obtener la confesión actual
+    res = supabase.table("confesion").select("estado").eq("id", id).single().execute()
+    if not res.data:
+        return  # No existe esa confesión
+
+    estado_actual = res.data["estado"]
+    nuevo_estado = not estado_actual
+
+    # Actualizar el estado en la base de datos
+    supabase.table("confesion").update({"estado": nuevo_estado}).eq("id", id).execute()
